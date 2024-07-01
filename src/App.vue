7ec059036e8c9df213f06d25a2c337c87c7f1a51
Mbalentlediko <dikombalentle9@gmail.com>
2024-07-01 09:09:09 +0200

    Footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
-</template>
-
-<style>
+  <div class="wrapper">
+   <NavBar/>
+   <main class="container-fluid">
+      <router-view/> 
+      </main>
+     <Footer/>
+   </div>
+ </template>
+ 
+ <script>
+ import NavBar from '@/components/NavBar'
+ import Footer from '@/components/Footer.vue'
+ export default {
+   components:{
+     NavBar,
+     Footer
+   },
+   computed:{
+     jobTitle() {
+      return this.$store.state.jobTitle
+     },
+     about() {
+       return this.$.store.state.about
+     },
+     education() {
+       return this.$.store.state.education
+     }
+   },
+ }
+ </script>
+ <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer class="container-fluid">
+      <div class="row">
+          <p class="lead">
+              Mbalentle's portfolio &copy; <span>{{ cYear }}</span>
+          </p>
+      </div> 
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+   name: "FooterComp",
+   data() {
+      return {
+      cYear: new Date().getUTCFullYear()
+   }
+  }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
