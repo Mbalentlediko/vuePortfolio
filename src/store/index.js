f07d16c47d152bdbeb7d0259238b33e3cd10ac3e
Mbalentlediko <dikombalentle9@gmail.com>
2024-07-01 08:30:17 +0200

    folderstructure

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,16 @@
 import { createStore } from 'vuex'
-
+// import axios from 'axios'
+// import Swal from 'sweetalert2/dist/sweetalert2' 
+// const portfolioURL = 'https://mbalentlediko.github.io/eompTask/data/'
 export default createStore({
   state: {
+    jobTitle: null,
+    about: null,
+    education: null,
+    experiences: null,
+    skills: null,
+    testimonials: null,
+    projects: null
   },
   getters: {
   },
