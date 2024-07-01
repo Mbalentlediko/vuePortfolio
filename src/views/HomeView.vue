aea7536fc1521979d02fdfb6ac00f34562fbbf7a
Mbalentlediko <dikombalentle9@gmail.com>
2024-07-01 09:25:30 +0200

    Landing Page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <section class="row" id="landing">
-  
+      <LandingPage/>
     </section>
     <section class="row" id="about"></section>
     <section class="row" id="education"></section>
@@ -13,8 +13,12 @@
 </template>
 
 <script>
+import LandingPage from '@/components/LandingPage.vue';
 export default {
-
+  name: 'HomeView',
+  components: {
+    LandingPage,
+  }
 }
 </script>
 
